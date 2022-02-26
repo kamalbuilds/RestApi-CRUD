0e2de16087b171a486154ce4af707b4bea99a459
Kamal_nayan <geniusamansingh@gmail.com>
2022-02-26 16:57:36 +0530

    complete rest API

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import userRoutes from './routes/users.js'
+import mongoose from 'mongoose';
 
+mongoose.connect('mongodb://localhost:27017/RestApi',{})
+.then(()=> console.log('mongodb connected..'));
 const app =express();
 const PORT =5000;
 
 app.use(bodyParser.json());
 // all routes start with '/users' eg if it is '/' == '/users'
 app.use('/users', userRoutes);
-app.get('/',(req,res)=>res.send('hello  kamal nayan'));
+app.get('/',(req,res)=>res.send('hello  kamal nayan Welcome'));
 app.listen(PORT, ()=>console.log(`Server ${PORT}`));
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from 'express';
-import { uuid } from 'uuidv4';
+import { uuid } from 'uuidv4';//unique no
 import { v4 as uuidv4 } from 'uuid';
 const router =express.Router();
-const users=[
-    {
-        firstName:"kamal",
-        LastName:"nayan",
-        age: 25
-    },
-    {
-        firstName:"vidhika",
-        LastName:"jha",
-        age: 19
-    }
-]
+const o='List of users ';
+let users=[];
 
 router.get('/',(req,res)=>{
     res.send(users);
@@ -25,6 +15,42 @@ router.post('/',(req,res)=>{
     const user=req.body;
     // const userwithid= {...users}
     users.push({...user, id: uuidv4()});
-    res.send(`User with name ${user.LastName} added to the db`);
+    res.send(`User with name ${user.firstName} added to the db`);
 });
+
+router.get('/:id',(req,res)=>{
+    const {id} =req.params;
+    const foundUser =users.find((user)=> user.id=== id);
+    res.send(foundUser);
+})
+
+router.delete('/:id',(req,res)=>{
+    // const {id} =req.params;
+    const {id} =req.params;
+    const user=req.body;
+    users= users.filter((user)=> user.id !== id);
+    res.send(`User with id ${user.LastName} deleted`);
+})
+
+
+router.patch('/:id',(req,res)=>{
+    const {id} =req.params;
+    const {firstName, LastName, age} =req.body;
+
+    const user=users.find((user)=> user.id===id);
+    if(firstName) user.firstName= firstName;
+    if(LastName) user.LastName= LastName;
+    if(age) user.age= age;
+
+    res.send(`User with id ${id} updated`);
+})
+
+router.delete('/:id',(req,res)=>{
+    // const {id} =req.params;
+    const {LastName} =req.params;
+    const user=req.body;
+    users= users.filter((user)=> user.LastName === LastName);
+    res.send(`User with id ${user.LastName} deleted`);
+})
+
 export default router;
